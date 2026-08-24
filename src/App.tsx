f4285c104c64cb72067a8fde674a906fde2e634a
Math.aMalafaia <math.amalafaia@gmail.com>
2026-08-24 07:04:08 -0300

    Feat, Adição do React-Routes-Dom e compilação da pages Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 
 import './App.css'
-import Content from './components/Content/Content' 
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home'
 
 
 function App() {
 
   return (
-    <div className="app">
-      <Header />
-      <Content />
-      <Footer />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
   )
 }
 
