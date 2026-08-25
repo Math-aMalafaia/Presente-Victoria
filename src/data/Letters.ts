671eeb83c0116e3098525d5dfbd00db80f6ebde1
Math.aMalafaia <math.amalafaia@gmail.com>
2026-08-25 12:05:09 -0300

    Feat: Adição de mais uma carta]

--- src/data/Letters.ts
+++ src/data/Letters.ts
@@ -5,11 +5,24 @@ export const letters = [
         id: 1,
         title: "Quando Tudo Me Lembra Você",
         text: ["Sempre adimirei o silêcio.", "Sempre contemplei as mais belas obras.", "Sempre me encantei com o perfume das flores.", "Mas hoje já não sei o que dizer sobre elas",
-                    "Pois, no silêncio, só escuto tua voz.", "Nenhuma obra supera tua beleza.", "Entre todos os perfumes, o teu é o meu favorito.", "Por isso, faltam-me palavras", 
-                    "Sempre foi bela a lua iluminando a noite.", "Sempre admirei as emoções que a vida desperta.", "Sempre amei o nascer de cada manhã.", "Porém, nada disso se compara a ti",
-                    "Cada lua me faz lembrar teu sorriso.", "Cada emoção me conduz ao teu abraço.", "Cada alvorada reflete o brilho dos teus olhos.", "E é por isso que desejo falar",
-                    "Quero contar ao mundo.", "Compor canções e poemas.", "Trasformar em versos aquilo que sinto", "E declarar, sem medo, o meu por ti",
-                    "Eu Te Amo.", "três palavras.", "Incontáveis significados.", "Meu amor atravessa o ontem, o hoje e o amanhã.", "Como se existisse desde antes da criação do univeso", "e estivesse destinado a durar por toda a eternidade"
+                "Pois, no silêncio, só escuto tua voz.", "Nenhuma obra supera tua beleza.", "Entre todos os perfumes, o teu é o meu favorito.", "Por isso, faltam-me palavras", 
+                "Sempre foi bela a lua iluminando a noite.", "Sempre admirei as emoções que a vida desperta.", "Sempre amei o nascer de cada manhã.", "Porém, nada disso se compara a ti",
+                "Cada lua me faz lembrar teu sorriso.", "Cada emoção me conduz ao teu abraço.", "Cada alvorada reflete o brilho dos teus olhos.", "E é por isso que desejo falar",
+                "Quero contar ao mundo.", "Compor canções e poemas.", "Trasformar em versos aquilo que sinto", "E declarar, sem medo, o meu por ti",
+                "Eu Te Amo.", "três palavras.", "Incontáveis significados.", "Meu amor atravessa o ontem, o hoje e o amanhã.", "Como se existisse desde antes da criação do univeso", "e estivesse destinado a durar por toda a eternidade"
+            ]
+    },
+
+    {
+        id: 2,
+        title: "Aquela Que Eu Amo",
+        text: ["O meu Amor.", "A mulher main linda que existe.", "Os olhos mais lindos que rubis.", "A dona do meu coração.",
+                "A dona do sorriso main lindo que o amanhecer.", "A mulher com o cheiro mmais gostoso que o das flores.", "A dona do abraço que faz qualquer problema pareer pequeno.", 
+                "A voz que cosegue acalmar até os meus pensamentos mais barulhentos.", "O sorriso que me ilumina até os meus dias mais escuros.", "O olhar que me faz esquecer o mundo ao meu redor.",
+                "A pessoa que trasforma moments simples em memórias que eu quero guardar para sempre.", "A mulher que cosegue fazer meu coração bater mias forte apenas com um 'oi'",
+                "Aquela que, mesmo sem perceber, consegue deixar meus dias muito mais bonito", "Você é o meu lugar favorito, mesmo quando estamos longe.", "É o pensamento que aparece no meu rosto quando eu sorrio sozinho.",
+                "é a pessoa que eu quero ter ao meu lado nos dias bons, nos dias difíceis e em todos aqueles dias comuns que, com você, deixam de ser comuns", "Você é mais do que a mulher que eu amo.", "É a minha melhor lembrança, o meu presente favorito e uma das partes mais bonitas da minha vida.",
+                "E, se eu pudesse escolher novamente entre todas as pessoas deste mundo escolheria você.", "Hoje. Amanhã. E em todos os dias que ainda vierem"
             ]
     }
 ]
