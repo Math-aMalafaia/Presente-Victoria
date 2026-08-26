4d668656740ec4b670e4d8e196c084e6168f7461
Math.aMalafaia <math.amalafaia@gmail.com>
2026-08-26 09:11:25 -0300

    atualização parceial timeline

--- src/components/Content/Components-Content/TimeLine/TimeLine.tsx
+++ src/components/Content/Components-Content/TimeLine/TimeLine.tsx
@@ -1,43 +1,33 @@
 import './TimeLine.css';
 import PointTime from './PointTime';
+import { timeLine } from '../../../../data/TimeLine';
 
 function TimeLine(){
+
+     const timeLineList = Object.values(timeLine);
     return(
         <>
             <div className="timeLine">
-                <h2>Time Line</h2>
-                <p>Confira a nossa propria Linha do Tempo</p>
-
-                <PointTime 
-                title="Final do Ano"
-                data="31/12/25 & 01/01/26"
-                description="Neste dia, foi o começo de toda a nossa amizade"
-                />
+                <div className='timeLine-title'>
+                    <h2>Time Line</h2>
+                    <p>Confira a nossa propria Linha do Tempo</p>
+                </div>
                 
-                <PointTime 
-                title="Brasil x Marrocos"
-                data="13/06/26"
-                description="Neste dia devo dizer que eu ja  estva gostando de você"
-                />
-
-                <PointTime 
-                title="Quando eu te escolhi"
-                data="26/06/26"
-                description="É iconico esse ter sido seu aniversario, mas eu me refiro aquela conversar que tivemos"
-                />
+                <div className='timeLine-line'></div>
                 
-                <PointTime 
-                title="Aniversario"
-                data="26/06/26"
-                description="Não posso mentir neste dia, eu fiquei uma boa parte do tempo pensando se não poderiamos ficar jutos e ate dei alguma investidas"
-                />
-
-                <PointTime
-                img='src/assets/Memory/Somos-1.jpg'
-                title="Somos 1"
-                data="27/06/26"
-                description="Deve dizer que neste dia, eu fiquei muito ancioso para te enconra novamente"
-                />
+                <div className='timeLine-Text'>
+                    {(timeLineList.map(
+                        (timeLine) => (
+                            <PointTime 
+                                key={timeLine.year} 
+                                title={timeLine.title}
+                                data={timeLine.date}
+                                description={timeLine.description}
+                                img={timeLine.img}
+                            />
+                        )
+                    ))}
+                </div>
 
             </div>
         </>
---
+++ src/data/TimeLine.ts
@@ -0,0 +1,46 @@
+import Somos1 from "../assets/Memory/Somos-1.jpg";
+
+export const timeLine = [
+    {
+        year: 2025,
+        title: "Final de Ano | Começo da nossa historia",
+        date: "31/12/2025",
+        description: "Devo dizer, neste dias estavamos, iciniando tudo o  que amariamos masis um no outro"
+    },
+
+    {
+        year: 2026,
+        title: "Inicio do Ano | Começo da nossa hisstoria",
+        date: "01/01/2026",
+        description: "Não sei oq falar sobre esse dia, a não ser que deste de ali eu sabia que eu queriaa estar masis proximo de você, afinal eu tinha achado você muito massa, e por sinal Obrigado, pq por sua causa descobri que gosto de mutias coisas"
+    },
+
+    {
+        year: 2026,
+        title: "Brasil x Marrocos",
+        date: "13/06/2026",
+        description: "Neste dia devo dizer que eu já estva gostando de você"
+    }, 
+
+    {
+        year: 2026,
+        title: "Quando eu te escolhi",
+        date: "26/06/2026",
+        description: "É iconico esse ter sido seu aniversario, mas eu me refiro aquela conversar que tivemos"
+    }, 
+
+    {
+        year: 2026,
+        title: "Aniversario",
+        date: "26/06/2026",
+        description: "Não posso mentir neste dia, eu fiquei uma boa parte do tempo pensando se não poderiamos ficar jutos e ate dei alguma investidas"
+    },
+
+    {
+        year: 2026,
+        img: Somos1,
+        title: "Somos 1",
+        date: "27/06/2026",
+        description: "Você não imagian, mas neste dia, eu contava as horas para poder ti encontrar"
+    }
+]
